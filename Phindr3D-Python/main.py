6b489753ceb8f7dcee202a1569d33286085a82af
mschumak <ma.schumaker@gmail.com>
2022-05-06 18:18:08 -0400

    Add main.py, virtual environment

---
+++ Phindr3D-Python/main.py
@@ -0,0 +1,12 @@
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')
+
+
+# Main
+if __name__ == '__main__':
+    """Phindr3D is   """
+    print_hi('PyCharm')
+
+# end Main
