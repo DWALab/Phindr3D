a370493f6467a369a1d35ae4e5fc386aaa4f8420
frankly02 <lolfrankz@gmail.com>
2022-05-17 14:21:08 -0400

    Setup main window of GUI, no features implemented so far. Next step: Scroll bars/Sliders, Menu Bar, other windows, button implementations

--- Phindr3D-Python/src/GUI/MainGUI.py
+++ Phindr3D-Python/src/GUI/MainGUI.py
@@ -16,7 +16,7 @@
 
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
-from PyQt5.QtGui import QPixmap
+from PyQt5.QtGui import *
 
 class MainGUI:
     """Defines the main GUI window of Phindr3D"""
@@ -24,15 +24,18 @@ class MainGUI:
     def __init__(self):
         """MainGUI constructor"""
         self.app = QApplication([])
+        self.foundMetadata = False
         self.mainWindow = self.buildMainWindow()
 
+    # Function to build main running window
     def buildMainWindow(self):
         """Build the window widget and its components"""
+        mainWindow = QMainWindow()
+        mainWindow.setWindowTitle("Phindr3D")
         win = QWidget()
         layout = QGridLayout()
         layout.setAlignment(Qt.AlignBottom)
         win.setMinimumSize(0, 0)
-
 
         # All widgets initialized here, to be placed in their proper section of GUI
         loadmeta = QPushButton("Load MetaData")
@@ -45,6 +48,44 @@ class MainGUI:
         nextimage = QPushButton("Next Image")
         phind = QPushButton("Phind")
         # Button behaviour defined here
+        def metadataError():
+            if not self.foundMetadata:
+                alert = self.buildErrorWindow("Metadata not found!!", QMessageBox.Critical)
+                alert.exec()
+        # Currently, only error messages will appear, for set voxel button
+        setvoxel.clicked.connect(metadataError)
+
+        # Declaring menu actions, to be placed in their proper section of the menubar
+        menubar = QMenuBar()
+        file = menubar.addMenu("File")
+        imp = file.addMenu("Import")
+        impsession = imp.addAction("Session")
+        impparameters = imp.addAction("Parameters")
+        exp = file.addMenu("Export")
+        expsessions = exp.addAction("Session")
+        expparameters = exp.addAction("Parameters")
+
+        metadata = menubar.addMenu("Metadata")
+        createmetadata = metadata.addAction("Create Metafile")
+        loadmetadata = metadata.addAction("Load Metadata")
+
+        imagetab = menubar.addMenu("Image")
+        imagetabnext = imagetab.addAction("Next Image")
+        imagetabcolors = imagetab.addAction("Set Channel Colors")
+
+        viewresults = menubar.addAction("View Results")
+
+        about = menubar.addAction("About")
+
+        # Menu actions defined here
+        def extractMetadata():
+            winz = self.buildExtractWindow()
+            winz.show()
+            winz.exec()
+
+        createmetadata.triggered.connect(extractMetadata)
+        # Creating and formatting menubar
+        mainWindow.setMenuBar(menubar)
 
         # create analysis parameters box (top left box)
         analysisparam = QGroupBox("Analysis Parameters")
@@ -57,7 +98,7 @@ class MainGUI:
         grid.addWidget(adjust, 3, 0, 1, 2)
         analysisparam.setLayout(grid)
         analysisparam.setFixedSize(140, 180)
-        layout.addWidget(analysisparam, 0, 0)
+        layout.addWidget(analysisparam, 1, 0)
 
         # create image viewing parameters box (bottom left box)
         imageparam = QGroupBox("Image Viewing Parameters")
@@ -68,25 +109,98 @@ class MainGUI:
         vertical.addWidget(nextimage)
         imageparam.setLayout(vertical)
         imageparam.setFixedSize(140, 180)
-        layout.addWidget(imageparam, 1, 0)
+        layout.addWidget(imageparam, 2, 0)
 
         # Phind button
-        layout.addWidget(phind, 2, 0, Qt.AlignCenter)
+        layout.addWidget(phind, 3, 0, Qt.AlignCenter)
 
         # Box for image (?)
         imgwindow = QGroupBox()
         imgwindow.setFlat(True)
         img = QLabel()
+        # Set image to whatever needs to be displayed (temporarily set as icon for testing purposes)
         pixmap = QPixmap('C:\Program Files\Git\Phindr3D\phindr3d_icon.png')
+
         pixmap = pixmap.scaled(400, 400)
         img.setPixmap(pixmap)
         imagelayout = QVBoxLayout()
         imagelayout.addWidget(img)
         imgwindow.setLayout(imagelayout)
         imgwindow.setFixedSize(400, 400)
-        layout.addWidget(imgwindow, 0, 1, 3, 1)
+        layout.addWidget(imgwindow, 1, 1, 3, 1)
         win.setLayout(layout)
+        mainWindow.setCentralWidget(win)
+        return mainWindow
+
+    def buildErrorWindow(self, errormessage, icon):
+        alert = QMessageBox()
+        alert.setText(errormessage)
+        alert.setIcon(icon)
+        return alert
+
+    def buildExtractWindow(self):
+        largetext = QFont("Arial", 12, 1)
+        win = QDialog()
+        win.setWindowTitle("Extract Metadata")
+        directory = "Image Root Directory"
+        samplefilename = "Sample File Name"
+        layout = QGridLayout()
+        imagerootbox = QTextEdit()
+        imagerootbox.setReadOnly(True)
+        imagerootbox.setText(directory)
+        imagerootbox.setFixedSize(300,60)
+        imagerootbox.setAlignment(Qt.AlignHCenter | Qt.AlignTop)
+        imagerootbox.setFont(largetext)
+
+        selectimage = QPushButton("Select Image Directory")
+        selectimage.setFixedSize(130, 30)
+
+        samplefilebox = QTextEdit()
+        samplefilebox.setReadOnly(True)
+        samplefilebox.setText(samplefilename)
+        samplefilebox.setAlignment(Qt.AlignHCenter | Qt.AlignTop)
+        samplefilebox.setFont(largetext)
+        samplefilebox.setFixedSize(450, 40)
+
+        expressionbox = QLineEdit()
+        expressionbox.setAlignment(Qt.AlignHCenter | Qt.AlignTop)
+        expressionbox.setFont(largetext)
+        expressionbox.setFixedSize(450, 30)
+        expressionbox.setPlaceholderText("Type Regular Expression Here")
+
+        evaluateexpression = QPushButton("Evaluate Regular Expression")
+        evaluateexpression.setFixedSize(160, 30)
+
+        outputfilebox = QLineEdit()
+        outputfilebox.setAlignment(Qt.AlignCenter)
+        outputfilebox.setFont(largetext)
+        outputfilebox.setFixedSize(200, 30)
+        outputfilebox.setPlaceholderText("Output Metadata File Name")
+
+        createfile = QPushButton("Create File")
+        createfile.setFixedSize(100, 30)
+
+        cancel = QPushButton("Cancel")
+        cancel.setFixedSize(100, 30)
+
+        # button functions
+
+        cancel.clicked.connect(win.close)
+
+        layout.addWidget(imagerootbox, 0, 0, 1, 2)
+        layout.addWidget(selectimage, 0, 2, 1, 1)
+        layout.addWidget(samplefilebox, 1, 0, 1, 3)
+        layout.addWidget(expressionbox, 2, 0, 1, 3)
+        layout.addWidget(evaluateexpression, 3, 0, 1, 1)
+        layout.addWidget(outputfilebox, 4, 0, 1, 1)
+        layout.addWidget(createfile, 4, 1, 1, 1)
+        layout.addWidget(cancel, 4, 2, 1, 1)
+        layout.setSpacing(10)
+        win.setLayout(layout)
+        win.setFixedSize(500, 300)
         return win
+
+
 
     def run(self):
         """Show the window and run the application exec method to start the GUI"""
