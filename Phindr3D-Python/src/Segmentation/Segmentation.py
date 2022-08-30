2ba455128573ac61f26f07b39f16a59dfd73231b
Michael Schumaker <mschumak@users.noreply.github.com>
2022-08-30 11:36:59 -0400

    Change all copyright statements from 'src' to 'Phindr3D' (#88)
    * Add error handling if the user enters ImageID as a group name
    
    * Change all copyright statements from 'src' to 'Phindr3D'

--- Phindr3D-Python/src/Segmentation/Segmentation.py
+++ Phindr3D-Python/src/Segmentation/Segmentation.py
@@ -1,18 +1,18 @@
 # Copyright (C) 2022 Sunnybrook Research Institute
-# This file is part of src <https://github.com/DWALab/Phindr3D>.
+# This file is part of Phindr3D <https://github.com/DWALab/Phindr3D>.
 #
-# src is free software: you can redistribute it and/or modify
+# Phindr3D is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# src is distributed in the hope that it will be useful,
+# Phindr3D is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with src.  If not, see <http://www.gnu.org/licenses/>.
+# along with Phindr3D.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
 import json
